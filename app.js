07e32d0452b66c3030f33c6e1e66fe97f28b42aa
yusufadhiwinata <yusufadhiwinata80@gmail.com>
2025-06-13 02:43:24 +0800

    pus

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose'); // Import Mongoose
 const bcrypt = require('bcryptjs');
